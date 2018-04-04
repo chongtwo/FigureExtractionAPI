2e2fd132b874e1a5a61148ac47ad90f8b447116e
Wang Yuhong <358397459@qq.com>
2018-04-04 13:15:02 +0800

    simplify

--- src/main/java/com/springboot/two/service/FigureExtraction.java
+++ src/main/java/com/springboot/two/service/FigureExtraction.java
@@ -1,7 +1,6 @@
 package com.springboot.two.service;
 
 import com.springboot.two.domain.*;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import java.text.SimpleDateFormat;
@@ -15,42 +14,27 @@ import java.util.Map;
 @Component
 
 public class FigureExtraction {
-    public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        FigureExtraction figureExtraction = new FigureExtraction();
-//        ArrayList<String> longSentenceList = TxtOperator.readTxt(".\\static\\CT胸部平扫约4000份-描述.txt");
-        ArrayList<String> longSentenceList = TxtOperator.readTxt(".\\out\\distinctSentence2018-03-28-14-37-41.txt");
-//        ArrayList<String> longSentenceList = new ArrayList<>();longSentenceList.add("左肺下叶可见多发结节状高密度影，");
-        figureExtraction.go(longSentenceList);
-        long endTime = System.currentTimeMillis();
-        System.out.println("用时:"+ (endTime-startTime) + "ms");
-    }
-
 
     public ArrayList<ArrayList<String>> go(ArrayList<String> longSentenceList) {
         int numOfLong = 0;
         ArrayList<ArrayList<String>> allList = new ArrayList<>();
-//        XlsOperator xlsOperator = new XlsOperator();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-//        String excelPath = "./out/result"+ String.valueOf(dateFormat.format(new Date())) + ".xlsx";
-        String excelPath = "./out/distinctresult"+ String.valueOf(dateFormat.format(new Date())) + ".xlsx";
         //写入表头
-        ArrayList<String> columnName = new ArrayList<>();
-        columnName.add("原句");
-        columnName.add("语义");
-        columnName.add("主干部位");
-        columnName.add("细节部位");
-        columnName.add("区域");
-        columnName.add("可能性");
-        columnName.add("性状");
-        columnName.add("诊断");
-        columnName.add("变化");
-        columnName.add("数量");
-        columnName.add("测量位置");
-        columnName.add("测量值");
-        columnName.add("单位");
-//        xlsOperator.writeXls(excelPath, columnName);
+//        ArrayList<String> columnName = new ArrayList<>();
+//        columnName.add("原句");
+//        columnName.add("语义");
+//        columnName.add("主干部位");
+//        columnName.add("细节部位");
+//        columnName.add("区域");
+//        columnName.add("可能性");
+//        columnName.add("性状");
+//        columnName.add("诊断");
+//        columnName.add("变化");
+//        columnName.add("数量");
+//        columnName.add("测量位置");
+//        columnName.add("测量值");
+//        columnName.add("单位");
 
+        //取一句长句，循环
         int end = longSentenceList.size();
         for (String longS : longSentenceList.subList(0, end)) {
             numOfLong++;
@@ -71,24 +55,24 @@ public class FigureExtraction {
                     columnContent = new ArrayList<>();
                     columnContent.add(ss.content);
                     columnContent.add(ss.semanticSentence);
+
                     StructuredShortSentence se = entry.getValue();
-                    columnContent.add(se.primaryLocation);
-                    columnContent.add(se.specificLocation);
-                    columnContent.add(se.region);
-                    columnContent.add(se.possibility);
-                    columnContent.add(se.descriptor);
-                    columnContent.add(se.diagnosis);
-                    columnContent.add(se.change);
-                    columnContent.add(se.quantifier);
-                    columnContent.add(se.measureLocation);
-                    columnContent.add(se.value);
-                    columnContent.add(se.unit);
+                    columnContent.add("主干部位：" + se.primaryLocation);
+                    columnContent.add("细节部位：" + se.specificLocation);
+                    columnContent.add("区域：" + se.region);
+                    columnContent.add("可能性：" + se.possibility);
+                    columnContent.add("性状：" + se.descriptor);
+                    columnContent.add("诊断：" + se.diagnosis);
+                    columnContent.add("变化：" + se.change);
+                    columnContent.add("数量：" + se.quantifier);
+                    columnContent.add("测量部位：" + se.measureLocation);
+                    columnContent.add("数值：" + se.value);
+                    columnContent.add("单位：" + se.unit);
 
                     allList.add(columnContent);
                 }
             }
         }
-//        xlsOperator.writeXls(excelPath, allList);
         return allList;
     }
 }
